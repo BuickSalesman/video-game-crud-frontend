ea1d3c797286a90988457c5f37c13520f27e6ed3
BuickSalesman <adam.odonnell.e@gmail.com>
2024-04-30 21:07:05 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Footer } from "./Footer";
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+      <Signup />
+      <Login />
+      <LogoutLink />
+    </div>
+  );
+}
+
+export default App;
