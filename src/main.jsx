fbb50dde7c3b9140922e45c0cc420b8b647e1a94
BuickSalesman <adam.odonnell.e@gmail.com>
2024-04-30 21:59:14 -0400

    Working on enviroment variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const baseURL = import.meta.env.DEV ? import.meta.env.BASE_API_URL_DEV : import.meta.env.BASE_API_URL_PROD;
+
+axios.defaults.baseURL = baseURL;
+
+console.log(baseURL);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
