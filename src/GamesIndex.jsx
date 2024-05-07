f93054ad2d760c77c3bc348acba98de94b934ff3
BuickSalesman <adam.odonnell.e@gmail.com>
2024-05-07 17:52:21 -0400

    added some styling

--- src/GamesIndex.jsx
+++ src/GamesIndex.jsx
@@ -1,15 +1,20 @@
 export function GamesIndex(props) {
   return (
-    <div>
-      <h1>All games</h1>
+    <div className="bg-gray-900 text-gray-300 min-h-screen p-5">
+      <h1 className="text-4xl font-bold text-blue-400 mb-5">All games</h1>
       {props.games.map((game) => (
-        <div key={game.id}>
-          <h2>{game.title}</h2>
-          <img src={game.image} />
-          <p>Description: {game.description}</p>
-          <p>Release date: {game.release_date}</p>
-          <p>Platform: {game.platform}</p>
-          <button onClick={() => props.onShowGame(game)}>More info</button>
+        <div key={game.id} className="mb-10 p-5 border border-gray-700 rounded-lg">
+          <h2 className="text-3xl text-white mb-3">{game.title}</h2>
+          <img src={game.image} className="w-full mb-3 rounded-lg shadow-lg" />
+          <p className="mb-1">Description: {game.description}</p>
+          <p className="mb-1">Release date: {game.release_date}</p>
+          <p className="mb-3">Platform: {game.platform}</p>
+          <button
+            onClick={() => props.onShowGame(game)}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            More info
+          </button>
         </div>
       ))}
     </div>
